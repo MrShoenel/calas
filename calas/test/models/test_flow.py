d40a1c40b2e66c9ce18a2cc9e78b87e503a0143c
Sebastian Hönel <sebastian.honel@lnu.se>
2025-02-25 21:06:25 +0100

    Add some testing to combining various losses

--- calas/test/models/test_flow.py
+++ calas/test/models/test_flow.py
@@ -106,3 +106,46 @@ def test_calas_repr():
     samp_cod = flow.make_CoD_batch_random_embedding(
         nominal=samp, classes=samp_classes, distr=None, num_dims=(8,8), mode=None, use_grad_dir=True, normalize=True)
     assert samp_cod.shape == (samp.shape[0], repr.embed_dim)
+
+    classes_nominal = torch.zeros(samp.shape[0], device=samp.device)
+    classes_anomaly = torch.ones(samp_cod.shape[0], device=samp_cod.device)
+    total_loss =\
+        flow.loss(x=samp, classes=classes_nominal) +\
+        flow.loss_embedded(embedded=samp_cod, classes=classes_anomaly) +\
+        repr.loss(x=samp)
+    
+    # The first loss is about the nominal samples under the nominal class. It also
+    # affects the representation because loss() calls self.repr.forward(...).
+    # The second loss does not affect the representation, because we are forwarding
+    # a sample that was already embedded and has previously been *detached* from
+    # the computational graph of the representation. In other words, in this case,
+    # the anomalous samples will not affect how the representation trains and it
+    # will *not* learn to reconstruct those inputs.
+    # The third loss is an extra output in which the representation learns to re-
+    # construct its inputs. All three loses here are KL divergences so we can add
+    # them together.
+    
+    print(total_loss)
+
+    # In the following, we change the first loss in order to *not* affect the re-
+    # presentation. While we are using it to create an embedding of our nominal
+    # batch, we are deliberately detaching it. Now the only loss that really has
+    # an impact on the representation is its own loss (here: the 3rd).
+    total_loss =\
+        flow.loss_embedded(embedded=repr.forward(x=samp).detach(), classes=classes_nominal) +\
+        flow.loss_embedded(embedded=samp_cod, classes=classes_anomaly) +\
+        repr.loss(x=samp)
+    
+    
+    # In this example, the reconstruction will actually learn to reconstruct the
+    # anomalous samples, because we are using it directly to reconstruct a sample
+    # that we have deliberately made worse before. However, note the *detach()*!
+    # Like in the other 3-way losses, the first two losses give us the contrast
+    # in shape of conditions that are then applied to the base distribution.
+    total_loss =\
+        flow.loss(x=samp, classes=classes_nominal) +\
+        flow.loss(x=repr.reconstruct(embeddings=samp_cod).detach(), classes=classes_anomaly) +\
+        repr.loss(x=samp)
+
+    
+    
