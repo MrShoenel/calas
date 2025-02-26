87830f7a864e0a8e0c6caa9a6b1c4cd2f8a0afd3
Sebastian Hönel <sebastian.honel@lnu.se>
2025-02-26 18:26:22 +0100

    Cosmetic changes

--- calas/models/repr.py
+++ calas/models/repr.py
@@ -21,11 +21,16 @@ class Representation(nn.Module, ABC):
 
     @abstractmethod
     def forward(self, x: Tensor) -> Tensor:
+        """Alias for `embed()`."""
+        ... # pragma: no cover
+    
+
+    def embed(self, x: Tensor) -> Tensor:
         """
         Takes input from the original data space (X) and embeds it in some custom
         representation that usually has a different number of dimensions.
         """
-        ... # pragma: no cover
+        return self.forward(x=x)
 
 
 
@@ -34,16 +39,18 @@ class RepresentationWithReconstruct(Representation):
     @abstractmethod
     def decoder(self) -> nn.Module:
         """
-        A module used to decode embeddings. The output must have the same
-        dimensionality as the input. The decoder is used during reconstruction,
-        as well as for computing the loss of this representation.
+        An extra module used to decode embeddings. Its output must have the same
+        dimensionality as the original input. The decoder is used during
+        reconstruction, as well as for computing the loss of this representation.
         """
         ... # pragma: no cover
 
 
     def reconstruct(self, embeddings: Tensor) -> Tensor:
         """
-        Takes as input embedded data and runs it through the decoder.
+        Takes as input embedded data and runs it through the decoder. The shape
+        of the given embeddings here is the same as the result from `forward()`
+        (or `embed()`).
         """
         result: Tensor = self.decoder.forward(embeddings)
         assert result.dim() == 2 and result.shape[0] == embeddings.shape[0] and result.shape[1] == self.input_dim
@@ -55,7 +62,7 @@ class RepresentationWithReconstruct(Representation):
 
         i = prepare(x)
 
-        i_prime = self.forward(x=x)
+        i_prime = self.embed(x=x)
         i_prime = self.decoder(i_prime)
         i_prime = prepare(i_prime)
 
