93b4d8545647eab7bc6b049842bb0366ec4406d1
Sebastian Hönel <sebastian.honel@lnu.se>
2025-02-26 18:27:26 +0100

    Refactor some tests and add test_spaces_and_calas()

--- calas/test/models/test_flow.py
+++ calas/test/models/test_flow.py
@@ -81,15 +81,15 @@ def test_calas():
     use_C = torch.tensor([0,0,0,0,0, 1,1,1,1,1])
     use_X = torch.rand((10,2))
 
-    b, _ = flow.x_to_b(x=use_X, classes=use_C)
-    x, _ = flow.b_to_x(b=b, classes=use_C)
+    b, _ = flow.e_to_b(embeddings=use_X, classes=use_C)
+    x, _ = flow.e_from_b(base=b, classes=use_C)
     assert torch.allclose(input=use_X, other=x)
 
 
-    samp, _, _ = flow.sample(5)
+    samp, _, _ = flow.sample_emb(5)
     assert samp.shape == (5,2)
 
-    loss = flow.loss(x=x, classes=use_C)
+    loss = flow.loss(input=x, classes=use_C)
 
 
 def test_calas_repr():
@@ -102,22 +102,24 @@ def test_calas_repr():
     samp = two_moons_rejection_sampling(nsamples=100)
     samp_classes = torch.where(samp[:, 0] < 0, 0, 1)
 
-    embeddings = flow.repr.forward(x=samp)
+    embeddings = flow.repr.embed(x=samp)
     assert embeddings.shape == (100, repr.embed_dim)
     
-    samp2 = flow.sample(n_samp=50)[0]
-    assert samp2.shape == (50, 2)
+    samp2 = flow.sample_emb(n_samp=50)[0]
+    assert samp2.shape == (50, sum((32,16,32)))
+    samp3 = flow.sample(n_samp=10)[0]
+    assert samp3.shape == (10, 2)
 
 
-    samp_cod = flow.make_CoD_batch_random_embedding(
+    samp_cod = flow.make_CoD_batch_random_emb(
         nominal=samp, classes=samp_classes, distr=None, num_dims=(8,8), mode=None, use_grad_dir=True, normalize=True)
     assert samp_cod.shape == (samp.shape[0], repr.embed_dim)
 
     classes_nominal = torch.zeros(samp.shape[0], device=samp.device)
     classes_anomaly = torch.ones(samp_cod.shape[0], device=samp_cod.device)
     total_loss =\
-        flow.loss(x=samp, classes=classes_nominal) +\
-        flow.loss_embedded(embedded=samp_cod, classes=classes_anomaly) +\
+        flow.loss(input=samp, classes=classes_nominal) +\
+        flow.loss_emb(embeddings=samp_cod, classes=classes_anomaly) +\
         repr.loss(x=samp)
     
     # The first loss is about the nominal samples under the nominal class. It also
@@ -138,8 +140,8 @@ def test_calas_repr():
     # batch, we are deliberately detaching it. Now the only loss that really has
     # an impact on the representation is its own loss (here: the 3rd).
     total_loss =\
-        flow.loss_embedded(embedded=repr.forward(x=samp).detach(), classes=classes_nominal) +\
-        flow.loss_embedded(embedded=samp_cod, classes=classes_anomaly) +\
+        flow.loss_emb(embeddings=repr.forward(x=samp).detach(), classes=classes_nominal) +\
+        flow.loss_emb(embeddings=samp_cod, classes=classes_anomaly) +\
         repr.loss(x=samp)
     
     
@@ -149,9 +151,34 @@ def test_calas_repr():
     # Like in the other 3-way losses, the first two losses give us the contrast
     # in shape of conditions that are then applied to the base distribution.
     total_loss =\
-        flow.loss(x=samp, classes=classes_nominal) +\
-        flow.loss(x=repr.reconstruct(embeddings=samp_cod).detach(), classes=classes_anomaly) +\
+        flow.loss(input=samp, classes=classes_nominal) +\
+        flow.loss(input=repr.reconstruct(embeddings=samp_cod).detach(), classes=classes_anomaly) +\
         repr.loss(x=samp)
 
+
+def test_spaces_and_calas():
+    torch.manual_seed(0)
+    repr = AE_UNet_Repr(input_dim=2, hidden_sizes=(32,16,32))
+    flow = CalasFlowWithRepr(num_classes=2, flows=make_flows(dim=repr.embed_dim), repr=repr)
     
+    samp = two_moons_rejection_sampling(nsamples=10)
+    samp_class = torch.full((10,), 0.)
     
+    e  = flow.x_to_e(input=samp)
+    b  = flow.e_to_b(embeddings=e, classes=samp_class)[0]
+    e1 = flow.e_from_b(base=b, classes=samp_class)[0]
+    x1 = flow.x_from_e(embeddings=e1)
+
+    assert torch.allclose(input=e, other=e1, atol=1e-7)
+    # WE CANNOT assert the following because the representation was not trained to reconstruct properly!
+    assert not torch.allclose(input=samp, other=x1) 
+    
+    # Most likely, we cannot create anomalies with lower likelihood at this
+    # point, simply because the flow is not yet trained and will therefore
+    # assign very low likelihoods to the given ID samples.
+    lower_lik = flow.make_linear_global_anomaly(input=samp, classes=samp_class, likelihood='decrease')
+
+    # However, that here should work!
+    higher_lik = flow.make_linear_global_anomaly(input=samp, classes=samp_class, likelihood='increase')
+    samp_lik = flow.log_rel_lik(x=samp, classes=samp_class)
+    assert torch.all(samp_lik < flow.log_rel_lik(x=higher_lik, classes=samp_class))
