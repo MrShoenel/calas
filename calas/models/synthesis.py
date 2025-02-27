bebb68f30cea70997a3d4d1ca83f0829a9dd6ac0
Sebastian Hönel <sebastian.honel@lnu.se>
2025-02-27 10:32:30 +0100

    Finish implementing Linear synthesis

--- calas/models/synthesis.py
+++ calas/models/synthesis.py
@@ -18,6 +18,7 @@ class Synthesis(Generic[T]):
         self.was_training = self.flow.training
         self.flow.eval()
         self.ng = torch.no_grad()
+        self.ng.__enter__()
         return self
     
     def __exit__(self, exc_type: Optional[type[BaseException]]=None, exc_value: Optional[BaseException]=None, traceback: Optional[TracebackType]=None) -> None:
@@ -31,9 +32,11 @@ class Linear(Synthesis[T]):
 
     @staticmethod
     def _icdf_safe(q: Tensor, tol: float=1e-7) -> Tensor:
+        if q.dtype.itemsize < 4:
+            tol = 1e-2
         return torch.clip(input=q, min=tol, max=1.-tol)
 
-    def _modify(self, sample: Tensor, sample_is_embedding: bool, classes: Tensor, target_lik: float, condition: Literal['concentrate', 'lower_than', 'greater_than'], concentrate_dim_log_tol: float=1.0) -> Tensor:
+    def _modify(self, sample: Tensor, sample_is_embedding: bool, classes: Tensor, target_lik: float, condition: Literal['concentrate', 'lower_than', 'greater_than'], concentrate_dim_log_tol: float=1.0, return_all: bool=False) -> Tensor:
         flow = self.flow
         assert isinstance(sample, Tensor) and sample.dim() == 2 and sample.shape[0] > 1, 'sample needs to be a 2D Tensor with two or more data points.'
         if not sample_is_embedding:
@@ -42,23 +45,24 @@ class Linear(Synthesis[T]):
         
         
         normals = { clz: Normal(
-            loc=torch.full(size=(flow.num_dims,), fill_value=flow.mean_for_class(clazz=clz)),
-            scale=torch.full(size=(flow.num_dims,), fill_value=flow.scale_for_class(clazz=clz))) for clz in range(flow.num_classes) }
+            loc=flow.mean_for_class(clazz=clz).repeat(flow.num_dims),
+            scale=torch.exp(flow.log_scale_for_class(clazz=clz).repeat(flow.num_dims))) for clz in range(flow.num_classes) }
 
 
-        def to_b(t: Tensor, clz: Tensor) -> Tensor:
+        def to_b(t: Tensor, clz: Tensor) -> tuple[Tensor, Tensor]:
             if not sample_is_embedding:
                 t = flow.x_to_e(input=t)
-            return flow.e_to_b(embeddings=t, classes=clz).detach_()
+            b, ld = flow.e_to_b(embeddings=t, classes=clz)
+            return b, ld
         
         def from_b(b: Tensor, clz: Tensor) -> Tensor:
             t = flow.e_from_b(base=b, classes=clz)[0]
             if not sample_is_embedding:
                 t = flow.x_from_e(embeddings=t)
-            return t.detach_()
+            return t
         
         def lik(t: Tensor, clz: Tensor) -> Tensor:
-            return (flow.log_rel_lik_emb(embeddings=t, classes=clz) if sample_is_embedding else flow.log_rel_lik(input=t, classes=clz)).detach_()
+            return (flow.log_rel_lik_emb(embeddings=t, classes=clz) if sample_is_embedding else flow.log_rel_lik(input=t, classes=clz))
         
         def lik_unagg(t: Tensor, clz: Tensor) -> Tensor:
             if not sample_is_embedding:
@@ -67,7 +71,7 @@ class Linear(Synthesis[T]):
 
             return torch.vstack(tensors=list(
                 normals[clz_int[idx].item()].log_prob(value=b[idx]) for idx in range(t.shape[0])
-            )).detach_(), (b_log_det / b.shape[1]).unsqueeze(dim=0).T.repeat(1, b.shape[1]).detach_()
+            )), (b_log_det / b.shape[1]).unsqueeze(dim=0).T.repeat(1, b.shape[1])
 
         
         results: list[Tensor] = []
@@ -75,7 +79,7 @@ class Linear(Synthesis[T]):
         steps = 0
         while steps < 20 and len(results) < sample.shape[0]:
             steps += 1
-            b_batch = to_b(t=sample, clz=clz_int)[0].detach()
+            b_batch = to_b(t=sample, clz=clz_int)[0]
             u = 1e-5 + torch.rand_like(b_batch) * 0.003 # ~(0, 0.3]% change/step
 
             q = torch.vstack(tensors=list(
@@ -90,7 +94,7 @@ class Linear(Synthesis[T]):
             
             # The following lambda tells us about which side of the normal distr.
             # we're on. One could also say that it indicates the sign of the gradient.
-            lamb = torch.where(q < 0.5, 1., -1.)
+            lamb = torch.where(q < 0.5, 1., -1.).to(dtype=q.dtype)
             if condition == 'concentrate':
                 b_batch_unagg_likelihood, b_batch_unagg_log_det = lik_unagg(t=sample, clz=clz_int)
                 b_batch_unagg_likelihood += b_batch_unagg_log_det
@@ -120,18 +124,21 @@ class Linear(Synthesis[T]):
             clz_int = clz_int[~mask_accept]
             sample = samp_prime[~mask_accept]
         
-        if len(results) == 0:
-            return torch.empty((0, sample.shape[1]))
+        if len(results) < sample.shape[0]:
+            if return_all:
+                results.append(samp_prime) # Return even those that were not accepted
+            if len(results) == 0:
+                return torch.empty((0, sample.shape[1]))
         return torch.vstack(tensors=results)
         
 
 
-    def modify(self, sample: Tensor, classes: Tensor, target_lik: float, condition: Literal['concentrate', 'lower_than', 'greater_than'], concentrate_dim_log_tol: float=1.0) -> Tensor:
-        return self._modify(sample=sample, sample_is_embedding=False, classes=classes, target_lik=target_lik, condition=condition, concentrate_dim_log_tol=concentrate_dim_log_tol)
+    def modify(self, sample: Tensor, classes: Tensor, target_lik: float, condition: Literal['concentrate', 'lower_than', 'greater_than'], concentrate_dim_log_tol: float=1.0, return_all: bool=False) -> Tensor:
+        return self._modify(sample=sample, sample_is_embedding=False, classes=classes, target_lik=target_lik, condition=condition, concentrate_dim_log_tol=concentrate_dim_log_tol, return_all=return_all)
 
 
-    def modify_emb(self, sample: Tensor, classes: Tensor, target_lik: float, condition: Literal['concentrate', 'lower_than', 'greater_than'], concentrate_dim_log_tol: float=1.0) -> Tensor:
-        return self._modify(sample=sample, sample_is_embedding=True, classes=classes, target_lik=target_lik, condition=condition, concentrate_dim_log_tol=concentrate_dim_log_tol)
+    def modify_emb(self, sample: Tensor, classes: Tensor, target_lik: float, condition: Literal['concentrate', 'lower_than', 'greater_than'], concentrate_dim_log_tol: float=1.0, return_all: bool=False) -> Tensor:
+        return self._modify(sample=sample, sample_is_embedding=True, classes=classes, target_lik=target_lik, condition=condition, concentrate_dim_log_tol=concentrate_dim_log_tol, return_all=return_all)
 
 
 
--- calas/test/models/test_synthesis.py
+++ calas/test/models/test_synthesis.py
@@ -1,31 +1,40 @@
 import torch
-from torch import nn, Tensor
+from torch import nn, Tensor, device, cuda
 from normflows.flows import Flow, AutoregressiveRationalQuadraticSpline, LULinearPermute
 from calas.models.flow import CalasFlow, CalasFlowWithRepr
 from calas.tools.two_moons import two_moons_rejection_sampling, two_moons_likelihood, pure_two_moons_likelihood
-from calas.models.repr import RepresentationWithReconstruct
+from calas.models.repr import ReconstructableRepresentation
 from calas.models.synthesis import Linear
 from typing import override, Sequence
 from calas.test.models.test_flow import AE_UNet_Repr, make_flows
 
 
 
+dev = device('cuda' if cuda.is_available() else 'cpu')
+dty = torch.float32
+
+
 
 def test_linear():
-    repr = AE_UNet_Repr(input_dim=2, hidden_sizes=(32,16,32))
-    assert repr.embed_dim == 80
-    assert repr._decoder.in_features == sum((32,16,32)) and repr._decoder.out_features == 2
+    torch.manual_seed(0)
+    repr = AE_UNet_Repr(input_dim=2, hidden_sizes=(3,2,3))
+    repr.eval() # Put in eval mode, so we get deterministic behavior (has dropout!)
+    assert all(not p.training for p in repr.__dict__.values() if isinstance(p, nn.Module))
+    assert repr.embed_dim == 8
+    assert repr._decoder.out_features == 2
 
     flow = CalasFlowWithRepr(num_classes=2, flows=make_flows(dim=repr.embed_dim), repr=repr)
+    flow.to(device=dev, dtype=dty)
 
-    samp = two_moons_rejection_sampling(nsamples=10)
-    samp_class = torch.full((10,), 0.)
+    samp = two_moons_rejection_sampling(nsamples=10).to(device=dev, dtype=dty)
+    samp_class = torch.full((10,), 0.).to(device=dev, dtype=dty)
 
     with Linear(flow=flow) as linear:
-        avg_lik = flow.log_rel_lik(input=samp, classes=samp_class).mean().item()
+        avg_lik = flow.log_rel_lik(input=samp, classes=samp_class).mean()
 
         conc = linear.modify(
-            sample=samp, classes=samp_class, target_lik=avg_lik, condition='concentrate', concentrate_dim_log_tol=1.5)
+            sample=samp, classes=samp_class, target_lik=avg_lik, condition='concentrate', concentrate_dim_log_tol=1.5,
+            return_all=True)
         
         max_dev = repr.embed_dim * 1.5
         conc_lik = flow.log_rel_lik(input=conc, classes=samp_class)
