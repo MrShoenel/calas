0dbb052c132777a06d24d46aa705dadef87bd6ee
Sebastian Hönel <sebastian.honel@lnu.se>
2025-03-06 12:18:02 +0100

    Consolidate tests for Normal2Normal_NoGrad

--- calas/data/permutation_test.py
+++ calas/data/permutation_test.py
@@ -22,7 +22,7 @@ from ..models.synthesis_test import trained, untrained
 
 @mark.parametrize('space', [Space.Data, Space.Embedded, Space.Base])
 @mark.parametrize('lik', [Likelihood.Increase, Likelihood.Decrease])
-@mark.parametrize('fitted', [True, False])
+@mark.parametrize('fitted', [True, False], ids=['fit', 'not_fit'])
 def test_PermuteData_in_space(space: Space, lik: Likelihood, fitted: bool):
     torch.manual_seed(0)
     flow, samp, samp_class = (trained if fitted else untrained).all
@@ -48,7 +48,7 @@ def test_PermuteData_in_space(space: Space, lik: Likelihood, fitted: bool):
 
 
 @mark.parametrize('lik', [Likelihood.Increase, Likelihood.Decrease])
-@mark.parametrize('fitted', [True, False])
+@mark.parametrize('fitted', [True, False], ids=['fit', 'not_fit'])
 def test_N2N_Grad(lik: Likelihood, fitted: bool):
     torch.manual_seed(0)
     flow, samp, samp_class = (trained if fitted else untrained).all
@@ -78,31 +78,7 @@ def test_N2N_no_Grad(lik: Likelihood, base_grad: bool, fitted: bool, method: Lit
     # otherwise, this move naiive method will overshoot and fail to confidently
     # push the likelihood towards the desired direction.
     use_u = 0.001 if fitted and not base_grad else 0.01
-    n2n_ng = Normal2Normal_NoGrad(flow=flow, seed=0, u_min=use_u, u_max=use_u, stds_step_perc=0.025, use_loc_scale_base_grad=base_grad, method='loc_scale', grad_scaling=GradientScaling.Normalize)
-    perm_b = n2n_ng.permute(batch=samp_b, classes=samp_class, likelihood=lik)
-
-    num_corr = (flow.log_rel_lik_B(samp_b, samp_class) < flow.log_rel_lik_B(perm_b, samp_class)).sum() if lik == Likelihood.Increase else (flow.log_rel_lik_B(samp_b, samp_class) > flow.log_rel_lik_B(perm_b, samp_class)).sum()
-
-    assert num_corr == samp.shape[0]
-        
-
-
-@mark.parametrize('lik', [Likelihood.Increase, Likelihood.Decrease])
-def test_N2N_no_Grad_quantiles(lik: Likelihood):
-    torch.manual_seed(0)
-    repr = AE_UNet_Repr(input_dim=2, hidden_sizes=(3,2,3))
-    flow = CalasFlowWithRepr(num_classes=2, flows=make_flows(dim=repr.embed_dim), repr=repr).to(device=dev, dtype=dty)
-    samp = two_moons_rejection_sampling(nsamples=100).to(device=dev, dtype=dty)
-    samp_class = torch.full((100,), 0.).to(device=dev)
-    samp_b = flow.X_to_B(input=samp, classes=samp_class)[0]
-
-    # The 'quantiles' method works good with increased locs_scales_flow_frac and
-    # often quite bad of locs_scales_flow_frac close to one, esp. during early
-    # training when the forward-pushed base data is far from where it should be,
-    # because then the quantiles are too extreme, which is problematic esp. when
-    # we wish to decrease the likelihood. In other words, the more far off the
-    # data is, the lower the fraction should be.
-    n2n_ng = Normal2Normal_NoGrad(flow=flow, seed=0, locs_scales_flow_frac=0.1, u_min=0.01, u_max=0.01, stds_step_perc=0.025, use_loc_scale_base_grad=False, method='quantiles')
+    n2n_ng = Normal2Normal_NoGrad(flow=flow, seed=0, u_min=use_u, u_max=use_u, stds_step_perc=0.025, use_loc_scale_base_grad=base_grad, method=method, grad_scaling=GradientScaling.Normalize)
     perm_b = n2n_ng.permute(batch=samp_b, classes=samp_class, likelihood=lik)
 
     num_corr = (flow.log_rel_lik_B(samp_b, samp_class) < flow.log_rel_lik_B(perm_b, samp_class)).sum() if lik == Likelihood.Increase else (flow.log_rel_lik_B(samp_b, samp_class) > flow.log_rel_lik_B(perm_b, samp_class)).sum()
